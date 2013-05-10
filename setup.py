d6f523463aa04c4aa7058c603f4964f3a1022eed
Joshua D. Boyd <jdboyd@jdboyd.net>
2013-05-10 01:57:11 -0400

    Rough in main, and setup.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='irccat3',
+      version='0.1',
+      description='IRCCat in Python',
+      author='Joshua D. Boyd',
+      author_email='jdboyd@jdboyd.net',
+      url='https://github.com/jd-boyd/irccat3',
+      packages=find_packages(),
+      install_requires=[],
+      tests_require=['nose'],
+      entry_points={'console_scripts': ['irccat3 = irccat3.main:main',]
+     )
