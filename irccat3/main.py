32c199640aad657e9d6361daa88eac64bc40ff23
Joshua D. Boyd <jdboyd@jdboyd.net>
2013-05-15 01:58:14 -0400

    Give a default irc server of localhost.  Try a signal handler for clean exit, not working.

--- irccat3/main.py
+++ irccat3/main.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 import multiprocessing
 import Queue
+import signal
 import sys
 import threading
 
@@ -17,9 +18,10 @@ def get_args(argv):
 
     parser.add_argument("--verbosity", help="increase output verbosity")
 
-    parser.add_argument("--irc-server", help="increase output verbosity")
+    parser.add_argument("--irc-server", help="increase output verbosity",
+                        default='localhost')
     parser.add_argument("--irc-port", help="increase output verbosity",
-                        type=int, default=6666)
+                        type=int, default=6667)
 
     parser.add_argument("--listen-interface", help="increase output verbosity",
                         default='0.0.0.0')
@@ -30,22 +32,32 @@ def get_args(argv):
 
     return args
 
+l = None
+
+def signal_handler(signal, frame):
+    log.info('SIG %r', signal)
+    l.stop()
+    sys.exit(0)
+
 def main():
 
-    logging.basicConfig(level=logging.DEBUG)
-    log.error('TP')
+    logging.basicConfig(level=logging.INFO)
 
     args = get_args(sys.argv[1:])
 
+    log.info('args: %r', args)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
     q = Queue.Queue()
-    #q = multiprocessing.Queue()
     
     log.info('pre l start')
-    l = listener.Listener(q)
+    l = listener.Listener(q, host=args.listen_interface, 
+                          port=args.listen_port)
     l.start()
     log.info('past l start')
 
-    b = bot.Bot(q)
+    b = bot.Bot(q, host=args.irc_server, port=args.irc_port)
     b.start()
     log.info('past b start')
 
@@ -53,6 +65,3 @@ def main():
     b.join()
     
 
-# process options
-# start bot in one thread
-# start socket listener in another.
